c8fca512d7302ec18ba42c74377a7b7888457141
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-02 17:09:47 +0530

    Add files via upload

---
+++ add_two_num.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+//Program to add two number
+package Java_Git;
+public class add_two_num {
+    public static void main(String[]args)
+    {
+        int a=3;
+        int b=8;
+        int c=a+b;
+        System.out.println("the result is:"+c);
+}
+}
