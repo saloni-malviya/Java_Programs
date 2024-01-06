83ded99a3041cff8922fb2441d9bd6357ff4706a
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-06 18:18:57 +0530

    Add files via upload

---
+++ Size_of_operator.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Java_Git;
+
+public class Size_of_operator {
+    public static void main(String[]args)
+    {
+        System.out.println("The Size of 'BYTE' Datatype is: "+Byte.SIZE);
+        System.out.println("The Max Value of 'BYTE' Datatype is: "+Byte.MAX_VALUE);
+        System.out.println("The Min Value of 'BYTE' Datatype is: "+Byte.MIN_VALUE);
+        System.out.println("----------------------");
+        System.out.println("The Size of 'Short' Datatype is: "+Short.SIZE);
+        System.out.println("The Max Value of 'Short' Datatype is: "+Short.MAX_VALUE);
+        System.out.println("The Min Value of 'Short' Datatype is: "+Short.MIN_VALUE);
+        System.out.println("-----------------------");
+        System.out.println("The Size of 'Integer' Datatype is: "+Integer.SIZE);
+        System.out.println("The Max Value of 'Integer' Datatype is: "+Integer.MAX_VALUE);
+        System.out.println("The Min Value of 'Integer' Datatype is: "+Integer.MIN_VALUE);
+        System.out.println("-----------------------");
+        System.out.println("The Size of 'Long' Datatype is: "+Long.SIZE);
+        System.out.println("The Max Value of 'Long' Datatype is: "+Long.MAX_VALUE);
+        System.out.println("The Min Value of 'Long' Datatype is: "+Long.MIN_VALUE);
+        System.out.println("------------------------------------------------------------- ");
+        
+        
+        
+    }
+    
+}
