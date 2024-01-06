95c4110141ef234c35a7bb95ad684f3f581eb73b
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-06 18:18:30 +0530

    Add files via upload

---
+++ Math_fun.java
@@ -0,0 +1,24 @@
+
+package Java_Git;
+public class Math_fun {
+    public static void main(String[]args)
+    {
+        System.out.println("Value of PI is: "+Math.PI);
+        System.out.println("Value of Eular number is: "+Math.E);
+        System.out.println("Floor Value of 1.03 is: "+Math.floor(1.03));
+        System.out.println("Floor Value of -1.03 is: "+Math.floor(-1.03));
+        System.out.println("Ceil Value of 1.03 is: "+Math.ceil(1.03));
+        System.out.println("Ceil Value of -1.03 is: "+Math.ceil(-1.03));
+        System.out.println("Absolute Value of 5 is: "+Math.abs(5));
+        System.out.println("Absolute Value of -5 is: "+Math.abs(-5));
+        System.out.println("floormod value of 15,4 is: "+Math.floorMod(15,4));
+        System.out.println(15/4.0);
+        System.out.println("floorDiv Value of 15,4 is: "+Math.floorDiv(15,4));
+        System.out.println("Square value of 9 is: "+Math.sqrt(9));
+        System.out.println(Math.log(10));
+        System.out.println(Math.log10(10));
+        System.out.println(Math.exp(2));
+        System.out.println(Math.pow(3,4));
+    }
+    
+}
