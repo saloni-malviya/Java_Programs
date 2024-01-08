dda4a3eaca55586ed994bdda5db410eab96fa497
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-08 19:15:13 +0530

    Add files via upload

---
+++ Pattern5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Java_Git;
+
+/**
+ *
+ * @author Onsite Solution
+ */
+public class Pattern5 {
+    public static void main(String[]args)
+    {
+        int i,j;
+    System.out.println("....Pattern 5....");
+        for(i=5;i>=1;i--)
+        {
+            for(j=i;j<=5;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+}
+}
