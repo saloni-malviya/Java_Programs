920893bbe253302cacf09a9acf99e8c855ac683a
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-09 22:54:30 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Java_Git;
+
+/**
+ *
+ * @author Onsite Solution
+ */
+public class Pattern8 {
+    public static void main(String[]args)
+    {
+        int i,j;
+        
+    System.out.println("....Pattern 8....");
+        for(i=1;i<=5;i++)
+        {
+            for(j=i;j<=5;j++)
+            {
+                System.out.print(j);
+                
+            }
+            System.out.println();
+        }
+       // System.out.println("hello");
+        }
+    }
+    
+
