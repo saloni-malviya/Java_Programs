91bd36bc1c05d9a4315a5480e1e4454f00149f0a
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-09 22:54:52 +0530

    Add files via upload

---
+++ Pattern9.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Java_Git;
+
+/**
+ *
+ * @author Onsite Solution
+ */
+public class Pattern9 {
+    public static void main(String[]args)
+    {
+        int i,j;
+     System.out.println("....Pattern 9....");
+        for(i=5;i>=1;i--)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(i);
+                 }
+            System.out.println();
+           
+        }
+}
+}
